1aa14d8a956a06b483799abaa418f49c86c539bd
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-10-06 16:28:11 -0600

    i add a register form

--- src/app/auth/register/register.ts
+++ src/app/auth/register/register.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsersService } from '../../services/users';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './register.html',
-  styleUrl: './register.scss'
+  styleUrls: ['./register.scss'],
 })
 export class Register {
+  username = '';
+  email = '';
+  password = '';
+  confirmPassword = '';
+  message = '';
+  error = '';
 
+  constructor(private usersService: UsersService, private router: Router) {}
+
+  onRegister() {
+    if (this.password !== this.confirmPassword) {
+      this.error = 'Las contraseñas no coinciden';
+      this.message = '';
+      return;
+    }
+
+    const registered = this.usersService.register(
+      this.username,
+      this.password,
+      this.email
+    );
+
+    if (registered) {
+      this.message = 'Usuario registrado correctamente ✅';
+      this.error = '';
+      setTimeout(() => this.router.navigate(['/login']), 1500);
+    } else {
+      this.error = 'El usuario o correo ya existen ❌';
+      this.message = '';
+    }
+  }
 }
