ea48c55b87d7a74d27bec02e6157c50d1d1ee9c0
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-10-06 14:15:59 -0600

    Configuradas rutas globales: Auth + Main con arquitectura standalone

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./auth/auth-routing-module').then(m => m.routes),
+  },
+  {
+    path: 'main',
+    loadChildren: () =>
+      import('./main/main-routing-module').then(m => m.routes),
+  },
+  { path: '**', redirectTo: '' },
+];
